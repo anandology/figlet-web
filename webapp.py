704cc705962f5acc7f17ccce56a08e4294fc49d3
Anand Chitipothu <anandology@gmail.com>
2022-03-15 17:53:01 +0530

    first version of the figlet-web

---
+++ webapp.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+import subprocess
+
+app = Flask(__name__)
+
+def figlet(text, width=100):
+    p = subprocess.Popen(["figlet", "-w", str(width), text], stdout=subprocess.PIPE)
+    return p.stdout.read().decode('utf-8')
+
+@app.route("/")
+def index():
+    text = request.args.get("text") or "Hello!"
+    banner = figlet(text)
+    return render_template("index.html", text=text, banner=banner)
+
+if __name__ == "__main__":
+    app.run()
