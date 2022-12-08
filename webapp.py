61b132a6355699fa130c634fb2df47b3d2702ead
Anand Chitipothu <anandology@gmail.com>
2022-12-08 09:05:42 +0530

    Added API to access figlet

--- webapp.py
+++ webapp.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, make_response
 import subprocess
 import os
 
@@ -6,9 +6,18 @@ app = Flask(__name__)
 
 FONT = os.getenv("FIGLET_FONT", "standard")
 
-def figlet(text, width=100):
-    p = subprocess.Popen(["figlet", "-f", FONT, "-w", str(width), text], stdout=subprocess.PIPE)
+def figlet(text, width=100, font=FONT):
+    p = subprocess.Popen(["figlet", "-f", font, "-w", str(width), text], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     return p.stdout.read().decode('utf-8')
+
+@app.route("/api/figlet")
+def api_figlet():
+    text = request.args.get("text") or "Hello!"
+    font = request.args.get("font") or FONT
+    output = figlet(text, font=font)
+    response = make_response(output, 200)
+    response.mimetype = 'text/plain'
+    return response
 
 @app.route("/")
 def index():
